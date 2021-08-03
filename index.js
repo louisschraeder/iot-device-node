e8e4564602f3d94158a54d3b42f1dfaf5be3d979
louisschraeder <70850352+louisschraeder@users.noreply.github.com>
2021-08-03 21:20:05 +0200

    temp18

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function exec() {
         );
 
          */
-        const json = '{ "temp": "${res.temperature.toFixed(1)}" , "hum": "${res.humidity.toFixed(1)}"}';
+        const json = '{ "temp": "'+ res.temperature.toFixed(1) +'" , "hum": "' + res.humidity.toFixed(1) +'"}';
         const obj = JSON.parse(json);
         console.log(obj);
         console.log(obj.temp);
